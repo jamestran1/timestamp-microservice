dc13d3e55d3ba63b56c134a307b5f663a0bd3828
Glitch (bumpy-ink) <none>
2019-02-13 06:09:24 +0000

    URL Shorterner Microservice

--- server.js
+++ server.js
@@ -3,11 +3,14 @@
 
 // init project
 const express = require('express');
+const bodyParser = require('body-parser');
+const dns = require('dns');
 const app = express();
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
@@ -50,6 +53,41 @@ app.get('/api/whoami', (req, res) => {
     "software": req.header('User-Agent')
   });
 });
+
+var shorternUrls = [];
+var count = 1;
+
+app.post('/api/shorurl/new', (req, res) => {
+  var url = req.body.url.substring(8, req.body.url.length);
+  
+  dns.lookup(url, (err, address, family) => {
+    if (err) res.json({"error": "invalid URL"});
+    
+    for (var i=0; i< shorternUrls.length; i++) {
+      if (shorternUrls[i].original_url == url) {
+        res.json(shorternUrls[i]);
+      }
+    }
+    
+    var shortUrl = {
+      "original_url": url,
+      "short_url": count
+    };
+    shorternUrls.push(shortUrl);
+    count++;
+    console.log(shorternUrls);
+    res.json(shortUrl);
+  })
+  
+});
+
+app.get('/api/shorurl/new/:short', (req, res) => {
+  shorternUrls.forEach((d) => {
+      if (d.short_url == req.params.short) {
+        res.redirect("https://"+d.original_url);
+      }
+    });
+})
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
